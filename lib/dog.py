d4f0844285c7284fbc237c4bfd51ea2011bc2c98
maddox earles <mdxear@gmail.com>
2023-07-06 10:03:54 -0600

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+    
+    def bark(self):
+        print("Woof!")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+fido = Dog("Fido")
+fido.get_adopted("Sophie")
+
+
+snoopy = Dog("Snoopy")
